87d722ac51206e92108176b2efc0a5e52a4d47c0
Khusnianan <muhammadkhusni96@wastukancana.ac.id>
2025-04-11 13:59:30 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
 import psycopg2
+import streamlit as st
 
 def get_connection():
     return psycopg2.connect(
-        host="your-host",
-        database="your-db-name",
-        user="your-username",
-        password="your-password",
-        port="your-port"
+        host=st.secrets["DB_HOST"],
+        database=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASS"],
+        port=st.secrets["DB_PORT"]
     )
